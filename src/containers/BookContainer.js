3bdab8d13a38b0568e25c5ed81dfb00cf0d55127
David Lambert <dlambert124@utexas.edu>
2021-09-23 16:25:48 -0500

    added url and sucessfully console.log data

--- src/containers/BookContainer.js
+++ src/containers/BookContainer.js
@@ -1,37 +1,26 @@
 import CardComponent from "../components/CardComponent.js";
-import './BookContainer.css'
+import { useEffect, useState } from "react";
+import './BookContainer.css';
+import { Card } from "react-bootstrap";
+
+const HomeBooksAPI = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=D4oFfvNNYXE1xuXvVrb7ldJi34aceIZc`
 
 const BookContainer = () => {
+
+  const [ books, setBooks ] = useState([]);
+
+  useEffect(() => {
+    fetch(HomeBooksAPI)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBooks(data);
+      });
+  }, []);
+
   return (
-    <div className="cards_container">
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
+    <div>
+      {books.length > 0 && books.map((book) => <Card />)}
     </div>
   );
 };
